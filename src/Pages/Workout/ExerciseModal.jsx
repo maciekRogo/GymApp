cb7c26e5dfb03c718b65b68ad5dec5a093daced8
Mati <matizol@interia.pl>
2026-03-01 12:29:30 +0100

    Aktualizacja cwiczen (dodanie timera)

--- src/Pages/Workout/ExerciseModal.jsx
+++ src/Pages/Workout/ExerciseModal.jsx
@@ -1,12 +1,54 @@
+import React, { useState, useEffect } from 'react';
 import styles from "../../Components/css/ExerciseModal.module.css";
 
 const ExerciseModal = ({ exercise, onClose}) => {
+    // zmienne dla stopreka
+    const [timeLeft, setTimeLeft] = useState(60);
+    const [isTimerRunning, setIsTimerRunning] = useState(false);
+
+    // czyszczenie info oraz powrot do strony
+    useEffect(() => {
+        if (!exercise) {
+            setIsTimerRunning(false);
+            setTimeLeft(60);
+        }
+    }, [exercise]);
+
+    useEffect(() => {
+        let interval = null;
+
+        // sprawdza czy stoper jest lwaczony i odejmuje 1s
+        if (isTimerRunning && timeLeft > 0) {
+            interval = setInterval(() => {
+                setTimeLeft((prevTime) => prevTime - 1);
+            }, 1000);
+        } else if (timeLeft === 0) {
+            // po prostu zatrzymanie
+            setIsTimerRunning(false);
+            clearInterval(interval);
+        }
+
+        // sprzatacz po funkcji
+        return () => clearInterval(interval);
+    }, [isTimerRunning, timeLeft]);
+
+    const handleStartTimer = () => {
+        setTimeLeft(60); // ustawia 60s
+        setIsTimerRunning(true);
+    };
+
+    const handleClose = () => {
+        setIsTimerRunning(false); // wylacza stoper
+        setTimeLeft(60);
+        onClose();
+    };
+
     if (!exercise) return null;
 
     return (
-        <div className={styles.overlay} onClick={onClose}>
+        <div className={styles.overlay} onClick={handleClose}>
             <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-                <button className={styles.closeButton} onClick={onClose}>✕</button>
+                <button className={styles.closeButton} onClick={handleClose}>✕</button>
 
                 <h1>{exercise.name}</h1>
 
@@ -45,7 +87,18 @@ const ExerciseModal = ({ exercise, onClose}) => {
                     <p>{exercise.tips}</p>
                 </div>
 
-                <button className={styles.startButton}>Rozpocznij trening</button>
+                {/* wyswietlanie czasu - jestli dziala stoper to zmienia cyferki itd. */}
+                {isTimerRunning ? (
+                    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                        <h2 style={{ fontSize: '2rem', color: '#ff4444' }}>
+                            Pozostały czas: {timeLeft} s
+                        </h2>
+                    </div>
+                ) : (
+                    <button className={styles.startButton} onClick={handleStartTimer}>
+                        Rozpocznij trening
+                    </button>
+                )}
             </div>
         </div>
     );
